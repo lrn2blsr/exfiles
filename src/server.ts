ff975db4f62de93e6fc82e1a5fd1c79de62ac3a8
Mrufa <55004509+lrn2blsr@users.noreply.github.com>
2022-10-12 15:38:02 +0200

    changed limit for uploaded files

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,8 @@ import { createReadStream } from 'fs'
 
 const staticFiles = path.join(__dirname, 'static')
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.static(staticFiles))
 
 app.get('/', (req, res) => {
